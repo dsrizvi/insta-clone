219d6d73c0bac84cf1c5c2acb6c95396ffa4efba
sco77y <scottmunroe.jr@gmail.com>
2016-01-28 17:34:46 -0600

    m3

--- insta-clone/insta-clone/PhotosViewController .swift
+++ insta-clone/insta-clone/PhotosViewController .swift
@@ -8,8 +8,11 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    
+    var data : [NSDictionary]?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
